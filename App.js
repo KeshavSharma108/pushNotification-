afa30050f00fedcb68c7e69f6416bda5850c5fca
Keshav <sharkesh108@gmail.com>
2024-04-19 16:53:14 +0530

    commit

--- App.js
+++ App.js
@@ -1,7 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import React, { useEffect } from "react";
 export default function App() {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  }
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log(token);
+        });
+    } else {
+      console.log("Failed token status", authStatus);
+    }
+
+    async function getInitialURL() {
+      const url = await Linking.getInitialURL();
+      if (typeof url === "string") {
+        return url;
+      }
+      //getInitialNotification: When the application is opened from a quit state.
+      const message = await messaging().getInitialNotification();
+      const deeplinkURL = buildDeepLinkFromNotificationData(message?.data);
+      if (typeof deeplinkURL === "string") {
+        return deeplinkURL;
+      }
+    }
+
+    //onNotificationOpenedApp: When the application is running, but in the background.
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      const url = buildDeepLinkFromNotificationData(remoteMessage.data);
+      if (typeof url === "string") {
+        listener(url);
+      }
+    });
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      console.log("Message handled in the background!", remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return { unsubscribe, getInitialURL };
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +68,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
